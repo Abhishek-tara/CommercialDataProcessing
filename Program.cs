3a9d856deb583edba26215a1ec4658202b7ccde1
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-22 13:12:10 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CommercialDataProcessing
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Commercial Data Processing Program");
+        }
+    }
+}
